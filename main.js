646609efee7b03279f462ada2a65f9f59b8e2582
Casey Bond <volkswagentruck@gmail.com>
2017-11-14 20:30:19 -0600

    add book 2 object

---
+++ main.js
@@ -0,0 +1,9 @@
+var book 2 = {
+
+"id": 2,
+"name": "The life we bury",
+"author": "Allen Eskens",
+"picture_url": "https://images-na.ssl-images-amazon.com/images/I/51A4KkO7qzL._SX330_BO1,204,203,200_.jpg",
+"price": 12.39,
+"Description": "College student Joe Talbert has the modest goal of completing a writing assignment for an English class. His task is to interview a stranger and write a brief biography of the person. With deadlines looming, Joe heads to a nearby nursing home to find a willing subject."
+}
