afdfc0229e5b871675f5a1658177fcb83f66d7ff
Dillan Teagle <dillan.teagle.va@gmail.com>
2017-11-14 20:24:30 -0600

    book1

---
+++ main.js
@@ -0,0 +1,8 @@
+var book1 = {
+  "id" : 1,
+  "title" : "Girl with the Dragon Tattoo",
+  "author" : "Stieg Larsonn",
+  "price" : 8.60,
+  "description" : "This is a great book about a girl with a tattoo of a dragon"
+  "picture_url" : "https://images.gr-assets.com/books/1327868566l/2429135.jpg"
+}
